d8d8b4d207aff683a86522a19cbeb0cf665ac975
manuelpuga93 <manuelpuga93@gmail.com>
2018-04-12 23:37:21 -0500

    Se agregan archivos perdidos

---
+++ Contrats/Repositorios/ICompaniaRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comics.Contratos.Repositorios
+{
+    public interface ICompaniaRepositorio
+    {
+         
+    }
+}
